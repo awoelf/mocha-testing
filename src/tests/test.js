95765aa0dbd8fe31041542a9c0626c229d9a5c17
Alexis Woelffer <98348782+awoelf@users.noreply.github.com>
2023-04-14 17:29:05 -0500

    added the rest of the test cases

--- src/tests/test.js
+++ src/tests/test.js
@@ -3,7 +3,7 @@ import assert from "assert";
 
 describe("Math", function () {
   const math = new Math();
-  // Addition Function
+  // Addition Function a + b
   describe("Addition", function () {
     it("Should add the first argument to the second argument and return a number.", function () {
       let num1 = 100;
@@ -19,6 +19,8 @@ describe("Math", function () {
       assert.rejects(result);
     });
   });
+
+  // Subtraction Function a - b
   describe("Subtraction", function () {
     it("Should subtract the second argument from the first argument and return a number.", function () {
       let num1 = 40;
@@ -26,13 +28,84 @@ describe("Math", function () {
       let result = math.subtract(num1, num2);
       assert.equal(result, 5);
     });
+
+    it("Should return an error when undefined is used as an argument.", function () {
+      let num1 = undefined;
+      let num2 = 600;
+      let result = math.add(num1, num2);
+      assert.rejects(result);
+    });
   });
 
-  describe("Multiplication", function () {});
+  // Multiplication Function a * b
+  describe("Multiplication", function () {
+    it("Should multiply the first and second argument and return a number.", function () {
+      let num1 = 5;
+      let num2 = 7;
+      let result = math.multiply(num1, num2);
+      assert.equal(result, 35);
+    });
 
-  describe("Division", function () {});
+    it("Should return an error when null is used as an argument.", function () {
+      let num1 = null;
+      let num2 = -70;
+      let result = math.multiply(num1, num2);
+      assert.rejects(result);
+    });
+  });
 
-  describe("Modulo", function () {});
+  // Division Function a / b
+  describe("Division", function () {
+    it("Should divide the first by the second argument and return a number.", function () {
+      let num1 = 100;
+      let num2 = 20;
+      let result = math.divide(num1, num2);
+      assert.equal(result, 5);
+    });
 
-  describe("Factorial", function () {});
+    it("Should return an error when 0 is the divisor.", function () {
+      let num1 = 100;
+      let num2 = 0;
+      let result = math.divide(num1, num2);
+      assert.rejects(result);
+    });
+  });
+
+  // Modulo Function a % b
+  describe("Modulo", function () {
+    it("Should return the remainder as a number when the first argument is divided by the second argument.", function () {
+      let num1 = 20;
+      let num2 = 3;
+      let result = math.modulo(num1, num2);
+      assert.equal(result, 2);
+    });
+
+    it("Should return an error when 0 is the divisor.", function () {
+      let num1 = 10090;
+      let num2 = 0;
+      let result = math.modulo(num1, num2);
+      assert.rejects(result);
+    });
+  });
+
+  // Factorial function a!
+  describe("Factorial", function () {
+    it("Should return the factorial result of the argument.", function () {
+      let num1 = 5;
+      let result = math.factorial(num1);
+      assert.equal(result, 120);
+    });
+
+    it("Should return 1 as the factorial result when 0 is the argument.", function () {
+      let num1 = 0;
+      let result = math.factorial(num1);
+      assert.equal(result, 1);
+    });
+
+    it("Should return an error when a negative number is the argument.", function () {
+      let num1 = -1;
+      let result = math.factorial(num1);
+      assert.rejects(result);
+    });
+  });
 });
