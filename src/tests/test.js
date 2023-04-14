ce205d737f37bd064a3573d1ebf3d5d6929fe3f7
Alexis Woelffer <98348782+awoelf@users.noreply.github.com>
2023-04-14 00:51:49 -0500

    Created Readme

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,32 +1,38 @@
-import Math from '../utils/math.js';
-import assert from 'assert';
+import Math from "../utils/math.js";
+import assert from "assert";
 
-describe('Math', function () {
+describe("Math", function () {
   const math = new Math();
   // Addition Function
-  describe('Addition', function () {
-    it('Should add the first argument to the second argument and return a number.', function () {
+  describe("Addition", function () {
+    it("Should add the first argument to the second argument and return a number.", function () {
       let num1 = 100;
       let num2 = 50;
       let result = math.add(num1, num2);
       assert.equal(result, 150);
     });
-    
-    it('Should return an error when a string is used as an argument.', function () {
-      let num1 = '400';
+
+    it("Should return an error when a string is used as an argument.", function () {
+      let num1 = "400";
       let num2 = 60;
       let result = math.add(num1, num2);
       assert.rejects(result);
     });
-    describe('Subtraction', function () {});
-    
-    describe('Multiplication', function () {});
+  });
+  describe("Subtraction", function () {
+    it("Should subtract the second argument from the first argument and return a number.", function () {
+      let num1 = 40;
+      let num2 = 35;
+      let result = math.subtract(num1, num2);
+      assert.equal(result, 5);
+    });
+  });
 
-    describe('Division', function () {});
+  describe("Multiplication", function () {});
 
-    describe('Modulo', function () {});
+  describe("Division", function () {});
 
-    describe('Factorial', function () {});
+  describe("Modulo", function () {});
 
-  })
-})
+  describe("Factorial", function () {});
+});
